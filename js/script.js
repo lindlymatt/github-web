89bc8597d404ef8ed21efac245e14805b1b37933
Matthew Lindly <matthew.lindly@hotmail.com>
2016-11-18 23:11:36 -0700

    Finished.

--- js/script.js
+++ js/script.js
@@ -1,45 +1,37 @@
 'use strict';
 
-var $rightArrow = $('.arrow-1-r');
-var $pgTwoRAr;
-var $pgTwoLAr;
-var $pgThreeRAr;
-var $pgThreeLAr;
-var $pgFourLAr;
-var $body = $('#main-body');
+var $rightArrow = $('.arrow-right');
+var $leftArrow = $('.arrow-left');
+var pageNumber = 1;
+var $body = $('#text-section');
 
 $(() => {
-
   $rightArrow.on('click', () => {
-    $body.load("partials/step-two.partial", () => {
-      $pgTwoRAr = $('.arrow-2-r');
-      $pgTwoLAr = $('.arrow-2-l');
-
-      $pgTwoRAr.on('click', () => {
-        $pgThreeRAr = $('.arrow-3-r');
-        $pgThreeLAr = $('.arrow-3-l');
-
-        $pgThreeRAr.on('click', () => {
-          $body.load('partials/step-four.partial', () => {
-            $pgFourLAr = $('.arrow-4-l');
-
-            $pgFourLAr.on('click', () => {
-              $body.load('partials/step-three.partial');
-            });
-          });
-        });
-
-        $pgThreeRAr.on('click', () => {
-          $body.load('partials/step-two.partial');
-        });
-      });
-
-      $pgTwoRAr.on('click', () => {
-        $body.load("partials/step-one.partial", () => {
-          console.log('Loaded.');
-        });
-      });
-    });
+    pageNumber = pageNumber + 1;
+    if (pageNumber === 5) {
+      pageNumber = 4;
+      return console.log('Stop that!');
+    }
+    determinePage();
   });
-
+  $leftArrow.on('click', () => {
+    pageNumber = pageNumber - 1;
+    if (pageNumber === 0) {
+      pageNumber = 1;
+      return console.log('Stop that.');
+    }
+    determinePage();
+  });
 });
+
+function determinePage() {
+  if (pageNumber === 1) {
+    $body.load('partials/step-one.partial');
+  } else if (pageNumber === 2) {
+    $body.load('partials/step-two.partial');
+  } else if (pageNumber === 3) {
+    $body.load('partials/step-three.partial');
+  } else if (pageNumber === 4) {
+    $body.load('partials/step-four.partial');
+  }
+}
