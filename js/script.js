f222b40d836f368fe9cfe7c6bff4dd2eab6b598e
Matthew Lindly <matthew.lindly@hotmail.com>
2016-11-18 23:15:29 -0700

    Minor changes to title text, fonts, things like that.

--- js/script.js
+++ js/script.js
@@ -26,12 +26,24 @@ $(() => {
 
 function determinePage() {
   if (pageNumber === 1) {
-    $body.load('partials/step-one.partial');
+    $body.load('partials/step-one.partial', () => {
+      let $steps = $('.steps-text');
+      $steps.text('Step One: Make a Repo & Add Collaborators');
+    });
   } else if (pageNumber === 2) {
-    $body.load('partials/step-two.partial');
+    $body.load('partials/step-two.partial' () => {
+      let $steps = $('.steps-text');
+      $steps.text('Step Two: Clone, Pull & Branch');
+    });
   } else if (pageNumber === 3) {
-    $body.load('partials/step-three.partial');
+    $body.load('partials/step-three.partial' () => {
+      let $steps = $('.steps-text');
+      $steps.text('Step Three: Work, Add, Commit, Pull & Push');
+    });
   } else if (pageNumber === 4) {
-    $body.load('partials/step-four.partial');
+    $body.load('partials/step-four.partial' () => {
+      let $steps = $('.steps-text');
+      $steps.text('Step Four: Handle the Pull Request, Merge & Repeat');
+    });
   }
 }
